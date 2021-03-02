993a1e47d0e938ed95faf2cf0544bbd524db4e21
lingyun <chenlingyun@zhenrongbao.com>
2021-03-02 15:34:31 +0800

    增加module

---
+++ src/main/java/com/ling/module1/Sub1MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ling.module1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Sub1MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sub_activity_main)
+    }
+
+}
